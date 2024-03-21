975be8c8055f40d47f343588795720327e993903
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-21 16:06:19 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
 import Feed from './components/Feed'
+import Navbar from './components/Navbar'
 import Rightbar from './components/Rightbar'
 import Sidebar from './components/Sidebar'
-import { Container, } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 
 function App() {
   
 
   return (
     <>
-        <Container>
-            <Sidebar />
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+           <Sidebar />
             <Feed />
             <Rightbar />
-        </Container>
+        </Stack>
+        </Box>
     </>
   )
 }
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,7 +1,14 @@
+import { Box, } from '@mui/material'
 
 const Rightbar = () => {
   return (
-    <div>Rightbar</div>
+    <Box
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      Rightbar
+    </Box>
   )
 }
 
