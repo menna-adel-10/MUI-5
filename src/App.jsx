0faab26dda6e0ecaad554fb652e890f445676d5c
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-21 15:44:41 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Feed from './components/Feed'
+import Rightbar from './components/Rightbar'
+import Sidebar from './components/Sidebar'
+import { Container, } from '@mui/material'
 
 function App() {
   
 
   return (
     <>
+        <Container>
+            <Sidebar />
+            <Feed />
+            <Rightbar />
+        </Container>
     </>
   )
 }
