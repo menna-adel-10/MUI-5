6cfbaf3bec98d8ab0bb927bd6d2a382fe62a3a88
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-21 17:01:58 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,59 @@
+import { AccountTree, Mail, MailLock, Notifications,  } from '@mui/icons-material'
+import { AppBar, Toolbar, styled, Typography, InputBase, Avatar, Badge } from '@mui/material'
+
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius
+}));
+const Icons = styled("Box")(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex"
+  }
+}));
+const UserBox = styled("Box")(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none"
+  }
+}));
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="sticky" color='info'>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>
+          Instabook
+        </Typography>
+        <AccountTree sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+           <Badge badgeContent={134} color='error'>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ bgcolor: "black", width: 35, height: 35 }}>M</Avatar>
+        </Icons>
+        <UserBox>
+         <Avatar sx={{ bgcolor: "black", width: 35, height: 35 }}>M</Avatar>
+        </UserBox>
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
